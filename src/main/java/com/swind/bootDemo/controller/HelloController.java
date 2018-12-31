9de9e3e97e5359944b1246994cfb66c4bd2bfc0f
SLYEE <1529163084@qq.com>
2018-12-31 17:17:32 +0800

    dev分支第二次提交  创建了一个方法

--- src/main/java/com/swind/bootDemo/controller/HelloController.java
+++ src/main/java/com/swind/bootDemo/controller/HelloController.java
@@ -1,7 +1,19 @@
 package com.swind.bootDemo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by 孙立业 on 2018/12/31.
  */
+@RestController
+@RequestMapping(value = "hello")
 public class HelloController {
+
+    @RequestMapping(value = "")
+    public String hello () {
+        return "Hello world";
+    }
+
+
 }
